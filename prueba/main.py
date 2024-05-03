ae890fdac5858b3f3fc72e17603198a4da9b55e4
William Uncuyo <wmiranda696@gmail.com>
2024-05-03 18:00:30 -0300

    prueba 2

--- prueba/main.py
+++ prueba/main.py
@@ -1,13 +1,6 @@
-from search_word_in_file import *
-archivo1='archivo1.txt'
-archivo2='archivo2.txt'
-archivo3='archivo3.txt'
-archivo4='archivo4.txt'
-archivos=[archivo1,archivo2,archivo3,archivo4]
+from config import *
 
 print ("Generacion de una base de datos de documentos")
 texto=input("Busqueda: ")
 
-for file in archivos:
-    sheatch_text(texto,file)
-    print("------------------------------------------------------------------------------------")
+sheatch_text(texto)
